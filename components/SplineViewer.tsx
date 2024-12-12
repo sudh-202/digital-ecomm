e879624b555e16a1e0d08486dc2ec228c92cc77a
DigitallyNext <87563365+sudh-202@users.noreply.github.com>
2024-12-12 14:54:44 +0530

    the new udpate

--- components/SplineViewer.tsx
+++ components/SplineViewer.tsx
@@ -1,40 +1,50 @@
-'use client';
+// 'use client';
 
-import Script from 'next/script';
-import { useRef } from 'react';
+// import Script from 'next/script';
+// import { useRef } from 'react';
 
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      'spline-viewer': {
-        ref?: React.RefObject<HTMLElement>;
-        url?: string;
-        className?: string;
-      }
-    }
-  }
-}
+// declare global {
+//   namespace JSX {
+//     interface IntrinsicElements {
+//       'spline-viewer': {
+//         ref?: React.RefObject<HTMLElement>;
+//         url?: string;
+//         className?: string;
+//       }
+//     }
+//   }
+// }
 
-interface SplineViewerProps {
-  url: string;
-  className?: string;
-}
+// interface SplineViewerProps {
+//   url: string;
+//   className?: string;
+// }
 
-export default function SplineViewer({ url, className = '' }: SplineViewerProps) {
-  const viewerRef = useRef<HTMLElement>(null);
+// export default function SplineViewer({ url, className = '' }: SplineViewerProps) {
+//   const viewerRef = useRef<HTMLElement>(null);
 
+//   return (
+//     <>
+//       <Script 
+//         type="module" 
+//         src="https://unpkg.com/@splinetool/viewer@1.9.48/build/spline-viewer.js" 
+//         strategy="afterInteractive"
+//       />
+//       <spline-viewer
+//         ref={viewerRef}
+//         url={url}
+//         className={className}
+//       />
+//     </>
+//   );
+// }
+
+import React from 'react'
+
+const SplineViewer = () => {
   return (
-    <>
-      <Script 
-        type="module" 
-        src="https://unpkg.com/@splinetool/viewer@1.9.48/build/spline-viewer.js" 
-        strategy="afterInteractive"
-      />
-      <spline-viewer
-        ref={viewerRef}
-        url={url}
-        className={className}
-      />
-    </>
-  );
+    <div>SplineViewer</div>
+  )
 }
+
+export default SplineViewer
